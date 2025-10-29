a8338b966f382ec4cda8ab68bbafd26c2f1a5a7b
Justin Strackany <strackan@gmail.com>
2025-10-29 16:39:29 -0400

    fix: remove exports for deleted pricing components

---
+++ src/components/workflows/library/composite/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Composite Components Library
+ *
+ * These components combine multiple atomic components into
+ * cohesive, feature-rich UI blocks for workflows.
+ */
+
+// Pricing components removed - they depend on untracked files
+// Will be re-added when pricing optimization work is completed
